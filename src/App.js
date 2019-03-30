ebfd7f0cab007c037321c297c6d66100d0dc7db8
Mookeun Ji <goofcode@gmail.com>
2019-03-30 14:41:14 +0900

    Add react router dom and Create Header and Home

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Header from './components/Header';
+import Home from './pages/Home';
+
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-				</header>
-			</div>
+			<Router>
+				<div className="App">
+					<Header />
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route component={Home} />
+					</Switch>
+				</div>
+			</Router>
 		);
 	}
 }
