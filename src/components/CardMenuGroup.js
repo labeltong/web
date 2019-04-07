0147850a520c049b8fb3045fd081401831fa8d21
Mookeun Ji <goofcode@gmail.com>
2019-04-07 23:41:03 +0900

    Add onClick to cardmenus
    Add css for cardmenu link
    
    
    Add css for cardmenus

--- src/components/CardMenuGroup.js
+++ src/components/CardMenuGroup.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 class CardMenu extends Component {
 	render() {
@@ -7,21 +7,35 @@ class CardMenu extends Component {
 			<div className="menu-section">
 				<h5 className="title">Label by Method</h5>
 				<div className="row">
-					{this.props.menus.map(menu => (
-						<div className="card-menu col-md-4">
-							<div className="card">
-								<img className="card-img" src={menu.img} alt="Card menu" />
-								<div className="card-body">
-									<h5 className="card-title">{menu.title}</h5>
-									<p className="card-text">{menu.description}</p>
+					{this.props.menus.map(menu => {
+						let onClick = e => {
+							this.props.history.push(menu.link);
+						};
+						return (
+							<div className="card-menu col-md-4" key={menu.title}>
+								<div className="card">
+									<img
+										className="card-img"
+										src={menu.img}
+										alt="Card menu"
+										onClick={onClick}
+									/>
+									<div className="card-body">
+										<h5 className="card-title" onClick={onClick}>
+											{menu.title}
+										</h5>
+										<p className="card-text" onClick={onClick}>
+											{menu.description}
+										</p>
+									</div>
 								</div>
 							</div>
-						</div>
-					))}
+						);
+					})}
 				</div>
 			</div>
 		);
 	}
 }
 
-export default CardMenu;
+export default withRouter(CardMenu);
