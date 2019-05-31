76d34ca4d399378b7124c04202ed8504c47eaa0c
Mookeun Ji <goofcode@gmail.com>
2019-05-31 20:00:23 +0900

    Add audio library and sample audio

--- src/pages/AudioClassification.js
+++ src/pages/AudioClassification.js
@@ -1,53 +1,41 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import ReactAudioPlayer from 'react-audio-player';
 
 class AudioClassification extends Component {
-	state = { audio: '', options: [] };
-
-	componentDidMount() {
-		// TODO: update fetch url for audio source
-		// fetch audio
-		// fetch('http://54.180.195.179:19432/dataset/list/db_test/get').then(res => {
-		// 	res.json().then(data => {
-		// 		this.setState({
-		// 			audio: data.base_64_data,
-		// 			options: data.Dataq,
-		// 		});
-		// 	});
-		// });
-	}
+	state = {
+		audio:
+			'https://file-examples.com/wp-content/uploads/2017/11/file_example_WAV_1MG.wav',
+		options: ['sentiment1', 'sentiment2', 'sentiment3', 'sentiment4'],
+	};
 
 	render() {
 		return (
-			<div className="label-audio">
+			<div className="label-class">
 				<h5>Audio Classification</h5>
-				{this.state.img ? (
-					<div className="content">
-						<div className="image col-lg-7">
-							<audio src={`data:image/png;base64, ${this.state.img}`} />
-						</div>
-						<div className="control col-lg-4">
-							<div className="options">
-								{this.state.options.map(option => (
-									<div className="option" key={option}>
-										<input type="radio" name="class-option" value={option} />
-										{option}
-									</div>
-								))}
-							</div>
-							<button
-								className="btn"
-								onClick={() => {
-									this.props.history.push('/');
-								}}
-							>
-								Submit
-							</button>
-						</div>
+				<div className="content">
+					<div className="audio col-lg-7">
+						<ReactAudioPlayer src={this.state.audio} controls />
 					</div>
-				) : (
-					<div> Loading ... </div>
-				)}
+					<div className="control col-lg-4">
+						<div className="options">
+							{this.state.options.map(option => (
+								<div className="option" key={option}>
+									<input type="radio" name="class-option" value={option} />
+									{option}
+								</div>
+							))}
+						</div>
+						<button
+							className="btn"
+							onClick={() => {
+								this.props.history.push('/');
+							}}
+						>
+							Submit
+						</button>
+					</div>
+				</div>
 			</div>
 		);
 	}
