349d41093c30643f3769eed3896cfc6cc3a8f4c9
Mookeun Ji <goofcode@gmail.com>
2019-05-31 20:12:28 +0900

    Remove test image

--- src/testInputs.js
+++ src/testInputs.js
@@ -1,6 +1,3 @@
-import classificationImg from './static/ex_classification.png';
-import boundingImg from './static/ex_bounding.png';
-
 export const IntroNewsTestInput = [
 	{
 		title: 'LabelTong',
@@ -14,9 +11,4 @@ export const IntroNewsTestInput = [
 			'This is test event number 1. This is test event number 1. This is test event number 1.',
 		timestamp: '2019-03-20T10:35:24-08:00',
 	},
-	// {
-	// 	title: 'Test Event (2)',
-	// 	content: 'This is test event number 2. This is test event number 2.',
-	// 	timestamp: '2019-03-25T10:35:24-08:00',
-	// },
 ];
