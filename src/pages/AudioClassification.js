7c41b69403fcfcb86ad00377bb4b3e921d6b8f48
Mookeun Ji <goofcode@gmail.com>
2019-06-01 16:22:29 +0900

    Add audio

--- src/pages/AudioClassification.js
+++ src/pages/AudioClassification.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import ReactAudioPlayer from 'react-audio-player';
+import { getDataByMethod, submitAnswer } from '../api';
 
 class AudioClassification extends Component {
-	state = {
-		audio:
-			'https://file-examples.com/wp-content/uploads/2017/11/file_example_WAV_1MG.wav',
-		options: ['sentiment1', 'sentiment2', 'sentiment3', 'sentiment4'],
+	state = { id: null, audio: null, options: [], answer: null };
+
+	componentDidMount() {
+		getDataByMethod(3)
+			.then(data => {
+				this.setState({
+					id: data.ID,
+					audio: data.DataPath,
+					options: data.Question.split(','),
+				});
+			})
+			.catch(() => {
+				alert('로그인 해주세요');
+				this.props.history.push('/');
+			});
+	}
+
+	submit = () => {
+		if (this.state.answer) {
+			submitAnswer(this.state.id, this.state.answer);
+		}
+	};
+
+	onChangeAnswer = event => {
+		this.setState({ answer: event.target.value });
 	};
 
 	render() {
@@ -18,18 +40,25 @@ class AudioClassification extends Component {
 						<ReactAudioPlayer src={this.state.audio} controls />
 					</div>
 					<div className="control col-lg-4">
-						<div className="options">
-							{this.state.options.map(option => (
-								<div className="option" key={option}>
-									<input type="radio" name="class-option" value={option} />
-									{option}
-								</div>
-							))}
+						<div
+							className="options"
+							onChange={event => {
+								this.onChangeAnswer(event);
+							}}
+						>
+							{this.state.options.map((option, idx) => {
+								return (
+									<div key={idx} className="option">
+										<input type="radio" name="class-option" value={idx + 1} />
+										{option}
+									</div>
+								);
+							})}
 						</div>
 						<button
 							className="btn"
 							onClick={() => {
-								this.props.history.push('/');
+								this.submit();
 							}}
 						>
 							Submit
